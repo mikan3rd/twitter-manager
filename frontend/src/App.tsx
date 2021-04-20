d3cf9bec166b0f6a6c81cc51263a321906c1daf1
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-04-21 00:49:10 +0900

    chore: add bable config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { css } from "@emotion/react";
 import React from "react";
 
 export const App = () => {
   return (
     <div>
       <header>
-        <p>
+        <p
+          css={css`
+            background-color: red;
+          `}
+        >
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
